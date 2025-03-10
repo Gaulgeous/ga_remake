2aa388e4bebae8cdaf1c81f25f5f8601deea0afe
Gaulgeous <davidllgaul@gmail.com>
2025-03-10 14:15:55 +1000

    Added in docstrings and comments, as well as typecasting

--- src/ga_classification.py
+++ src/ga_classification.py
@@ -1,6 +1,8 @@
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 
+from sklearn.base import BaseEstimator
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import LinearSVC
 from sklearn.linear_model import LogisticRegression
@@ -10,23 +12,47 @@ from sklearn.tree import DecisionTreeClassifier
 from ga_base import GeneticAlgorithm
 from models import classification_models
 from performance_metric import calc_final_classification
+from typing import Dict, Any
 
-class GeneticAlgorithmRegressor (GeneticAlgorithm):
+class GeneticAlgorithmClassifier(GeneticAlgorithm):
+    """
+        Genetic Algorithm for classification problems
+        Inherits from base genetic algorithm class
+    """
 
+    def __init__(self, model: str, cols: int, population: int = 20, generations: int = 20, cv: int = 5, parents: int = 4) -> None:
+        """
+            Model initialisation
+            Inherits from the parent class initialisation, as well as introducing classification-specific parameters for the models used, and scoring metric
+            Creates an initial population for the first generation
 
-    def __init__(self, model, cols, population=20, generations=20, cv=5, parents=4):
+            Args:
+                model (str): Model to be used
+                cols (int): Number of columns in the dataset
+                population (int): Number of individuals in the population
+                generations (int): Number of generations
+                cv (int): Number of cross-validation splits
+                parents (int): Number of parents to be selected for the next generation
+        """
         super().__init__(model, cols, population, generations, cv, parents)
-
 
         self.models = classification_models
         self.scoring = "f1"
 
         self.create_population()
 
+    def create_model(self, genome: Dict[str, Any]) -> BaseEstimator:
+        """
+            Create a model based on the genome
+            overrides the parent class method for classification-specific problems
 
-    def create_model(self, genome):
+            Args:
+                genome (dict): Genome of the individual
 
-        model = None
+            Returns:
+                model: ML model to be used
+        """
+        model: BaseEstimator = None
 
         if self.model == "knn":
             model = KNeighborsClassifier(n_neighbors=genome['n_neighbors'], weights=genome['weights'], p=genome['p'])
@@ -40,16 +66,48 @@ class GeneticAlgorithmRegressor (GeneticAlgorithm):
             model = DecisionTreeClassifier(criterion=genome["criterion"], max_depth=genome["max_depth"], min_samples_split=genome["min_samples_split"], min_samples_leaf=genome["min_samples_leaf"])
         
         return model
-    
 
-    def predict(self, X_test, y_test):
+    def predict(self, X_test: pd.DataFrame, y_test: pd.Series) -> None:
+        """
+            Make predictions based on the best genome
+            overrides the parent class method for classification-specific problems
+            Prints the final performance metrics to terminal, and creates confusion matrix
 
-        X_train = self.clean_input_data(self.X_train, self.best_genome)
-        X_test = self.clean_input_data(X_test, self.best_genome)
+            Args:
+                X_test (pd.DataFrame): Test dataset
+                y_test (pd.Series): Test labels
+        """
+        X_train: np.ndarray = self.clean_input_data(self.X_train, self.best_genome)
+        X_test: np.ndarray = self.clean_input_data(X_test, self.best_genome)
 
-        model = self.create_model(self.best_genome["model"])
+        model: BaseEstimator = self.create_model(self.best_genome["model"])
 
         model.fit(X_train, self.y_train)
-        predictions = model.predict(X_test)
+        predictions: np.ndarray = model.predict(X_test)
 
         calc_final_classification(y_test, predictions)
+
+
+if __name__ == '__main__':
+    data_path: str = r"/home/david/Documents/git/ga_remake/data/dataset_phishing_reduced.csv"
+    df: pd.DataFrame = pd.read_csv(data_path)
+
+    # mapping = {'phishing': 1, 'legitimate': 0}
+    # column = df['status'].map(mapping)
+    # df['status'] = column
+
+    labels: pd.Series = df["status"]
+    data: pd.DataFrame = df.drop("status", axis=1)
+
+    X_train: pd.DataFrame
+    X_test: pd.DataFrame
+    y_train: pd.Series
+    y_test: pd.Series
+    X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.25, random_state=42)
+
+    print("Compiled")
+
+    genetic_algorithm: GeneticAlgorithmClassifier = GeneticAlgorithmClassifier("rf", cols=X_train.shape[1], parents=4, population=10, generations=10, cv=2)
+    genetic_algorithm.fit(X_train, y_train)
+    print("predicting")
+    genetic_algorithm.predict(X_test, y_test)
--- src/ga_regressor.py
+++ src/ga_regressor.py
@@ -1,6 +1,8 @@
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 
+from sklearn.base import BaseEstimator
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.linear_model import ElasticNet
 from sklearn.ensemble import RandomForestRegressor
@@ -9,11 +11,28 @@ from sklearn.tree import DecisionTreeRegressor
 from ga_base import GeneticAlgorithm
 from models import regression_models
 from performance_metric import calc_final_regression
+from typing import Dict, Any
 
-class GeneticAlgorithmRegressor (GeneticAlgorithm):
+class GeneticAlgorithmRegressor(GeneticAlgorithm):
+    """
+        Genetic Algorithm for regression problems
+        Inherits from base genetic algorithm class
+    """
 
+    def __init__(self, model: str, cols: int, population: int = 20, generations: int = 20, cv: int = 5, parents: int = 4) -> None:
+        """
+            Model initialisation
+            Inherits from the parent class initialisation, as well as introducing regression-specific parameters for the models used, and scoring metric
+            Creates an initial population for the first generation
 
-    def __init__(self, model, cols, population=20, generations=20, cv=5, parents=4):
+            Args:
+                model (str): Model to be used
+                cols (int): Number of columns in the dataset
+                population (int): Number of individuals in the population
+                generations (int): Number of generations
+                cv (int): Number of cross-validation splits
+                parents (int): Number of parents to be selected for the next generation
+        """
         super().__init__(model, cols, population, generations, cv, parents)
 
         self.models = regression_models
@@ -21,10 +40,18 @@ class GeneticAlgorithmRegressor (GeneticAlgorithm):
 
         self.create_population()
 
+    def create_model(self, genome: Dict[str, Any]) -> BaseEstimator:
+        """
+            Create a model based on the genome
+            overrides the parent class method for regression-specific problems
 
-    def create_model(self, genome):
+            Args:
+                genome (dict): Genome of the individual
 
-        model = None
+            Returns:
+                model: ML model to be used
+        """
+        model: BaseEstimator = None
 
         if self.model == "rf":
             model = RandomForestRegressor(n_estimators=genome["n_estimators"], max_features=genome["max_features"], min_samples_split=genome["min_samples_split"], min_samples_leaf=genome["min_samples_leaf"], bootstrap=genome["bootstrap"])
@@ -36,38 +63,48 @@ class GeneticAlgorithmRegressor (GeneticAlgorithm):
             model = KNeighborsRegressor(n_neighbors=genome["n_neighbors"], weights=genome["weights"], p=genome["p"])
         
         return model
-    
 
-    def predict(self, X_test, y_test):
+    def predict(self, X_test: pd.DataFrame, y_test: pd.Series) -> None:
+        """
+            Make predictions based on the best genome
+            overrides the parent class method for regression-specific problems
+            Prints the final performance metrics to terminal
 
-        X_train = self.clean_input_data(self.X_train, self.best_genome)
-        X_test = self.clean_input_data(X_test, self.best_genome)
+            Args:
+                X_test (pd.DataFrame): Test dataset
+                y_test (pd.Series): Test labels
+        """
+        X_train: np.ndarray = self.clean_input_data(self.X_train, self.best_genome)
+        X_test: np.ndarray = self.clean_input_data(X_test, self.best_genome)
 
-        model = self.create_model(self.best_genome["model"])
+        model: BaseEstimator = self.create_model(self.best_genome["model"])
 
         model.fit(X_train, self.y_train)
-        predictions = model.predict(X_test)
+        predictions: np.ndarray = model.predict(X_test)
 
         calc_final_regression(y_test, predictions)
 
 
-if __name__=='__main__':
-
-    data_path = r"/home/david/Documents/git/ga_remake/data/dataset_phishing_reduced.csv"
-    df = pd.read_csv(data_path)
+if __name__ == '__main__':
+    data_path: str = r"/home/david/Documents/git/ga_remake/data/dataset_phishing_reduced.csv"
+    df: pd.DataFrame = pd.read_csv(data_path)
 
     # mapping = {'phishing': 1, 'legitimate': 0}
     # column = df['status'].map(mapping)
     # df['status'] = column
 
-    labels = df["status"]
-    data = df.drop("status", axis=1)
+    labels: pd.Series = df["status"]
+    data: pd.DataFrame = df.drop("status", axis=1)
 
+    X_train: pd.DataFrame
+    X_test: pd.DataFrame
+    y_train: pd.Series
+    y_test: pd.Series
     X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.25, random_state=42)
 
     print("Compiled")
 
-    genetic_algorithm = GeneticAlgorithmRegressor("rf", cols=X_train.shape[1], parents=4, population=10, generations=10, cv=2)
+    genetic_algorithm: GeneticAlgorithmRegressor = GeneticAlgorithmRegressor("rf", cols=X_train.shape[1], parents=4, population=10, generations=10, cv=2)
     genetic_algorithm.fit(X_train, y_train)
     print("predicting")
     genetic_algorithm.predict(X_test, y_test)
